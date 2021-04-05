2dcb37c1f4c4ab8abfd060237f332a8420a53b74
Axonny <KawaiiAxonny@gmail.com>
2021-04-05 11:45:53 +0500

    add Trigger event script

---
+++ Assets/Scripts/PlayerTriggerEvent.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Collider2D))]
+public class PlayerTriggerEvent : MonoBehaviour
+{
+    public bool isEnter;
+    public UnityEvent onEnter;
+    public UnityEvent onExit;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isEnter = true;
+            onEnter.Invoke();
+        }
+    }
+    
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isEnter = false;
+            onExit.Invoke();
+        }
+    }
+}
