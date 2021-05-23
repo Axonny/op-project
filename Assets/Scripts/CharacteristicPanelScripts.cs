ace993483a089f1a2502ebca6521c12ace7f4176
timpo15 <timpo@mail.ru>
2021-05-23 20:08:43 +0300

    temp

---
+++ Assets/Scripts/CharacteristicPanelScripts.cs
@@ -0,0 +1,23 @@
+﻿public class CharacteristicPanelScripts
+{
+    public void IncreaseCharacteristic(int index)
+    {
+        Player.Instance.IncreaseCharacteristic(index);
+    }
+
+    public void DecreaseCharacteristic(int index)
+    {
+        Player.Instance.DecreaseCharacteristic(index);
+    }
+
+    public void SaveCharacteristics()
+    {
+        Player.Instance.SaveCharacteristics();
+    }
+
+    public void ResetCharacteristics()
+    {
+        Player.Instance.ResetCharacteristics();
+    }
+
+}
